eeddd69c2bc7799ab444a0286085a5ca1eaa95a3
Yannick PEROUX <yannick.peroux@gmail.com>
2014-02-28 16:09:49 +0000

    Fix fisheye effect

--- src/main.rs
+++ src/main.rs
@@ -4,37 +4,75 @@ use point::Point;
 use scene::{Scene, Spot};
 use shapes::{Object, Sphere, Plane};
 
-static WIDTH: int = 800;
-static HEIGHT: int = 800;
+static WIDTH: int = 1000;
+static HEIGHT: int = 1000;
 
 #[main]
 fn main() {
-  let mut pixels = [Black, ..((WIDTH * HEIGHT) as uint)];
+  let mut pixels = ~[Black, ..((WIDTH * HEIGHT) as uint)];
 
   let scene = Scene {
-    eye: Point::new(-1200., 0., 0.),
+    eye: Point::new(-12000., 0., 0.),
     spots: ~[
       Spot {
-        pos: Point::new(-1000., -100., 100.),
-        color: Blue
-      },
-      Spot {
-        pos: Point::new(-1000., 100., 100.),
-        color: Green
+        pos: Point::new(-1000., -200., 0.),
+        color: White
       },
     ],
     objects: ~[
       Object {
-        pos: Point::new(0., 0., 0.),
-        shininess: 0.8,
-        color: Black,
+        pos: Point::new(0., 400., 400.),
+        shininess: 0.,
+        color: Green,
         shape: ~Sphere { radius: 100. }
       },
       Object {
-        pos: Point::new(0., 0., -100.),
-        shininess: 0.8,
-        color: White,
-        shape: ~Plane
+        pos: Point::new(0., 400., 0.),
+        shininess: 0.,
+        color: Green,
+        shape: ~Sphere { radius: 100. }
+      },
+      Object {
+        pos: Point::new(0., 400., -400.),
+        shininess: 0.,
+        color: Green,
+        shape: ~Sphere { radius: 100. }
+      },
+      Object {
+        pos: Point::new(0., -400., 400.),
+        shininess: 0.,
+        color: Green,
+        shape: ~Sphere { radius: 100. }
+      },
+      Object {
+        pos: Point::new(0., -400., 0.),
+        shininess: 0.,
+        color: Green,
+        shape: ~Sphere { radius: 100. }
+      },
+      Object {
+        pos: Point::new(0., -400., -400.),
+        shininess: 0.,
+        color: Green,
+        shape: ~Sphere { radius: 100. }
+      },
+      Object {
+        pos: Point::new(0., 0., 400.),
+        shininess: 0.,
+        color: Green,
+        shape: ~Sphere { radius: 100. }
+      },
+      Object {
+        pos: Point::new(0., 0., 0.),
+        shininess: 0.,
+        color: Green,
+        shape: ~Sphere { radius: 100. }
+      },
+      Object {
+        pos: Point::new(0., 0., -400.),
+        shininess: 0.,
+        color: Green,
+        shape: ~Sphere { radius: 100. }
       },
     ]
   };
@@ -42,7 +80,7 @@ fn main() {
   for x in range(0, WIDTH) {
     for y in range(0, HEIGHT) {
       let vector = Point {
-        x: 1000.,
+        x: 10000.,
         y: (WIDTH / 2 - x) as f64,
         z: (HEIGHT / 2 - y) as f64
       };
